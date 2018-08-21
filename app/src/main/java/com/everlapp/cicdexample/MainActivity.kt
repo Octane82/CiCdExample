ffc205f6b6de33fbe6fc27f66a171051ead33b03
Dmitriy Karelin <dkarelin@bitum.ru>
2018-08-21 09:27:16 +0300

    First commit

---
+++ app/src/main/java/com/everlapp/cicdexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.everlapp.cicdexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *
+ * https://habr.com/post/328326/
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
